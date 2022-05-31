c1912ba95d6cffde3ddd58b98ccd86b27637dab1
Michael Echevarria <michael.echevarria@live.com>
2022-05-31 09:32:59 -0500

    updated code for js

--- js/charCount.js
+++ js/charCount.js
@@ -1,3 +1,29 @@
-exports.charCount = function() {
+exports.charCount = function(str) {
+    letters = {};
 
+    for (let i = 0; i < str.length; i++){
+        if (str[i] != " ") {
+            if(letters.hasOwnProperty(str[i])){
+                letters[str[i]] += 1;
+            } else {
+             letters[str[i]] = 1;
+            }
+        }
+    }
+    // made switch from sort arrow function to explicit sort function
+    // sort by count in descending order; same count, by alphabtical order
+    let sortDirection = -1;
+    let primarySort = [1];
+    let secondarySort = [0];
+    let sortedLetters = Object.entries(letters).sort(function(a, b) {
+        if (a[primarySort] == b[primarySort]) {
+            if(a[secondarySort] > b[secondarySort]) { return -sortDirection}
+            if(a[secondarySort] < b[secondarySort]) { return sortDirection}
+            if(a[secondarySort] == b[secondarySort]) { return 0}
+        }
+        if (a[primarySort] > b[primarySort]) { return sortDirection}
+        if (a[primarySort] < b[primarySort]) { return -sortDirection}
+    });
+
+    return sortedLetters;
 };
