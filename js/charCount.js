bc899a8b5aaf50d94096da707e0a51a24d8d2a32
Michael Echevarria <michael.echevarria@live.com>
2022-06-13 08:58:07 -0500

    updated js file

--- js/charCount.js
+++ js/charCount.js
@@ -1,6 +1,9 @@
 exports.charCount = function(str) {
+    // establishes object to contain each letter and count
     letters = {};
 
+    // using for loop to iterate through string; if character is whitespace, it is skipped.
+    // if string character is in letters object, the character count is incremented by 1; if not, character is added to letters object and value is set to 1
     for (let i = 0; i < str.length; i++){
         if (str[i] != " ") {
             if(letters.hasOwnProperty(str[i])){
@@ -15,6 +18,7 @@ exports.charCount = function(str) {
     let sortDirection = -1;
     let primarySort = [1];
     let secondarySort = [0];
+    // Object.entries returns an array of keys and values.
     let sortedLetters = Object.entries(letters).sort(function(a, b) {
         if (a[primarySort] == b[primarySort]) {
             if(a[secondarySort] > b[secondarySort]) { return -sortDirection}
@@ -25,5 +29,6 @@ exports.charCount = function(str) {
         if (a[primarySort] < b[primarySort]) { return -sortDirection}
     });
 
+    // return sortedLetters array
     return sortedLetters;
 };
